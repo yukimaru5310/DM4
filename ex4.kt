b3e8a25b924d2da168b93ebf2cdcdb0496f82e4f
yukimaru5310 <50508842+yukimaru5310@users.noreply.github.com>
2020-08-06 22:35:25 +0900

    最初のバージョン

---
+++ ex4.kt
@@ -0,0 +1,26 @@
+import java.util.concurrent.ThreadLocalRandom
+
+fun main(args: Array<String>) {
+     var heads = 0
+     var tails = 0
+     println("who are you?")
+     val name = readLine()
+     println("hello ${ name }!")
+
+     println("Tossing coin...")
+
+     for (i in 1..3) {
+          val random: Int = ThreadLocalRandom.current().nextInt(0, 2)
+          if(random == 1) {
+
+               println("Round${i}: head")
+               heads += 1
+          }else{
+               println("Round${i}: tail")
+               tails += 1
+          }
+     }
+     println("Heads: ${heads}, Tails: ${tails}")
+     if(tails < heads) println("${name} won!")
+     else println("${name} lose")
+}
